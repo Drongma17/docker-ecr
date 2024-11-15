d8be16116aa1d435bd90c368cbe327aa2d4b01e6
ngawang2yeshi@gmail.com <awan32600>
2024-11-15 16:13:47 -0600

    first commit

---
+++ src/main/java/com/thonglam/aws_cicd/dto/Course.java
@@ -0,0 +1,15 @@
+package com.thonglam.aws_cicd.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Course {
+
+    private int id;
+    private String name;
+    private double price;
+}
